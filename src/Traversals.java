cb79b0ffdd6c37ef719cafabadb690ffecd9447a
allenres <allen.resulidze@gmail.com>
2026-03-05 14:05:20 -0800

    complete

--- src/Traversals.java
+++ src/Traversals.java
@@ -9,26 +9,22 @@ public class Traversals {
     Node<Integer> child2 = new Node<>(58);
     Node<Integer> child3 = new Node<>(33);
 
-    root.children = new ArrayList<>();
     root.children.add(child1);
     root.children.add(child2);
     root.children.add(child3);
 
     Node<Integer> child1_1 = new Node<>(5);
     Node<Integer> child1_2 = new Node<>(99);
-    child1.children = new ArrayList<>();
     child1.children.add(child1_1);
     child1.children.add(child1_2);
 
 
     Node<Integer> child2_1 = new Node<>(73);
-    child2.children = new ArrayList<>();
     child2.children.add(child2_1);
 
     Node<Integer> child3_1 = new Node<>(24);
     Node<Integer> child3_2 = new Node<>(61);
     Node<Integer> child3_3 = new Node<>(12);
-    child3.children = new ArrayList<>();
     child3.children.add(child3_1);
     child3.children.add(child3_2);
     child3.children.add(child3_3);
@@ -36,8 +32,29 @@ public class Traversals {
 
     Node<Integer> child3_1_1 = new Node<>(83);
     Node<Integer> child3_1_2 = new Node<>(6);
-    child3_1.children = new ArrayList<>();
     child3_1.children.add(child3_1_1);
     child3_1.children.add(child3_1_2);
+
+    System.out.print(max(root));
+  }
+
+  static void preOrder(Node<?> node) {
+    if(node == null) return;
+    System.out.println(node.value);
+    for(Node<?> child : node.children) {
+      preOrder(child);
+    }
+  }
+
+  // returns the max value in the tree
+  // if node is null, return Integer.MIN_VALUE
+  static int max(Node<Integer> node) {
+    if(node == null) return Integer.MIN_VALUE;
+    int biggest = node.value;
+    for(Node<Integer> subtree : node.children) {
+      int subTreeBiggest = max(subtree);
+      if(subTreeBiggest > biggest) biggest = subTreeBiggest;
+    }
+    return biggest;
   }
 }
